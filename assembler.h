484b720980a4648c27f11ddff20645c49dbd3528
Gabe Chutuape <gfc2@linux-09.ews.illinois.edu>
2020-12-04 14:39:29 -0600

    Added assembler comments for abstraction

--- assembler.h
+++ assembler.h
@@ -5,6 +5,23 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
+/*
+BUILDUP/BREAKDOWN
+--------------------------------------------------------------------------------------
+Probably have some form of constructor 
+Probably have a default constructor as well just to be safe
+Probably have some form of destructor
+--------------------------------------------------------------------------------------
+FUNCTIONS
+--------------------------------------------------------------------------------------
+P1: DATA PROCESSING
+-Have something that reads the file data
+-Have something that puts it in a format we can use (Hashing?)
+P2: ACTUAL STUFF
+-Algorithm stuff. Will theorize later. Right now lets just get first two parts done
+-------------------------------------------------------------------------------------
+*/
+
 class Assembler{
 
 };
